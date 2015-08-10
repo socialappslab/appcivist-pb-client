5acffda0cd9d7795def3ce8211a15d7e65acd9af
Jose Antonio Moreno Punzon <joseanmp@gmail.com>
2015-08-10 22:02:54 +0200

    Creating data model and user interactions

--- app/controllers/controllers.js
+++ app/controllers/controllers.js
@@ -136,6 +136,23 @@ appCivistApp.controller('ForumCtrl', function($scope,$http){
 	});
 });
 
+appCivistApp.controller('CampaignCtrl', function($scope){
+	$scope.assemblies = [
+		{name:'Mairie de Paris', campaign:'PB Paris'},
+		{name:'Fête du Musique 2016', campaign:'PB Paris'},
+		{name:'Anti-Eviction Saint Suplice', campaign:'Assemblèe Droit au Logement'}
+	];
+	$scope.linkedAssembly = $scope.assemblies[1];
+
+	$scope.templates = [
+		{name: 'Participatory Budgeting'},
+		{name: 'Mobilization'},
+		{name: 'Community Organization'},
+		{name: 'Create your own...'}
+	];
+	$scope.selectedTemplate = $scope.templates[1];
+});
+
 appCivistApp.controller('TemplateConfigurationCtrl', function($scope){
 
 	$scope.proposalComponentsInputs = ['Proposal making', 'Versioning', 'Deliberation', 'Voting'];

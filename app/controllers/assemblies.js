3e19d9a229f27e846fdc5d58116bb844ac4f7410
Cristhian Parra <cdparra@gmail.com>
2015-08-15 23:38:16 -0700

    partially connected (without control and without assembly resources) to the assembly creation API

--- app/controllers/assemblies.js
+++ app/controllers/assemblies.js
@@ -2,7 +2,7 @@
 // with the $scope
 // The $scope is bound to the order view
 appCivistApp.controller('AssemblyListCtrl', function($scope, $routeParams,
-													 $resource, Assemblies, loginService, localStorageService) {
+													 $resource, $location, Assemblies, loginService, localStorageService) {
 
 	$scope.assemblies = [];
 	$scope.serverBaseUrl = localStorageService.get("serverBaseUrl");
@@ -14,7 +14,7 @@ appCivistApp.controller('AssemblyListCtrl', function($scope, $routeParams,
 		$scope.assemblies.$promise.then(function(data) {
 			$scope.assemblies = data;
 			localStorageService.set("assemblies", $scope.assemblies);
-			console.log("Assemblies arrived: " + JSON.stringify($scope.assemblies));
+			//console.log("Assemblies arrived: " + JSON.stringify($scope.assemblies));
 		});
 	}
 });
@@ -64,10 +64,48 @@ appCivistApp.controller('AssemblyCtrl',	function($scope, $routeParams, $resource
 		if (step === 1) {
 			console.log("Creating assembly with name = "+$scope.newAssembly.name);
 			console.log("Creating assembly with description = "+$scope.newAssembly.description);
+
+			if($scope.newAssembly.profile.membership.open) {
+				$scope.newAssembly.profile.supportedMembership="OPEN";
+			} else if ($scope.newAssembly.profile.membership.registration) {
+				if($scope.newAssembly.profile.membership.invitation &&
+					! $scope.newAssembly.profile.membership.request) {
+					$scope.newAssembly.profile.supportedMembership = "INVITATION";
+				} else if(! $scope.newAssembly.profile.membership.invitation &&
+					 $scope.newAssembly.profile.membership.request) {
+					$scope.newAssembly.profile.supportedMembership = "REQUEST";
+				} else if($scope.newAssembly.profile.membership.invitation &&
+					 $scope.newAssembly.profile.membership.request) {
+					$scope.newAssembly.profile.supportedMembership = "INVITATION_AND_REQUEST";
+				}
+			}
+
+			console.log("Creating assembly with membership = "+$scope.newAssembly.profile.supportedMembership);
+
+			if($scope.newAssembly.profile.roles.no) {
+				$scope.newAssembly.profile.managementType="OPEN";
+			} else if ($scope.newAssembly.profile.roles.yes) {
+				if($scope.newAssembly.profile.roles.coordinators &&
+					! $scope.newAssembly.profile.roles.moderators ) {
+					$scope.newAssembly.profile.managementType = "COORDINATED";
+				} else if(! $scope.newAssembly.profile.roles.coordinators &&
+					 $scope.newAssembly.profile.roles.moderators ) {
+					$scope.newAssembly.profile.managementType = "MODERATED";
+				} else if($scope.newAssembly.profile.roles.coordinators &&
+					 $scope.newAssembly.profile.roles.moderators ) {
+					$scope.newAssembly.profile.managementType = "COORDINATED_AND_MODERATED";
+				}
+			}
+
+			console.log("Creating assembly with managementType = "+$scope.newAssembly.profile.managementType);
+			localStorageService.set("temporaryNewAssembly",$scope.newAssembly)
 		} else if (step === 2) {
-			console.log("Creating new Assembly: " + $scope.newAssembly);
+			$scope.newAssembly = localStorageService.get("temporaryNewAssembly");
+			console.log("Creating new Assembly: " + JSON.stringify($scope.newAssembly));
 			var newAssembly = Assemblies.save($scope.newAssembly, function() {
 				console.log("Created assembly: "+newAssembly);
+				localStorageService.set("currentAssembly",newAssembly);
+				$location.url('/assembly/'+newAssembly.assemblyId+"/forum");
 			});
 		}
 	}

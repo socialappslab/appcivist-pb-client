9f50465a1e581e54b1017d921f63f99e94aaf545
Jose Antonio Moreno Punzon <joseanmp@gmail.com>
2015-12-31 00:54:51 +0100

    New Design of Main page ( First section )

--- app/i18n/app-strings.js
+++ app/i18n/app-strings.js
@@ -1,7 +1,14 @@
 appCivistApp.config(function ($translateProvider) {
     //English strings
     $translateProvider.translations('en', {
-        MAIN_TITLE: 'AppCivist is a platform for democratic assembly and collective action.'
+        MAIN_TITLE: 'AppCivist is a platform for democratic assembly and collective action.',
+        APPCIVIST_STRING: 'AppCivist',
+        LOG_IN: 'Log In',
+        SIGN_UP: 'Sign Up',
+        FOOTER_FIRST_LINE: 'By the Social Apps Lab at UC Berkeley',
+        FOOTER_SECOND_LINE: 'In partnership with the MiMove Team at INRIA, France'
+        //FOOTER_FIRST_LINE: 'By the <a href="http://socialappslab.org/">Social Apps Lab</a> at UC Berkeley',
+        //FOOTER_SECOND_LINE: 'In partnership with the "<a href="https://mimove.inria.fr/">MiMove Team</a>" at "<a href="http://www.inria.fr/">INRIA</a>", France'
     });
 
     //French strings

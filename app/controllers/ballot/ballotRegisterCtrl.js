a41f06441f7cdcd29e275d86c68d59f9842d39e2
Dmitri Skjorshammer <dmitriskj@gmail.com>
2016-01-08 18:10:13 -0800

    Simplifies ballotRegisterCtrl and adds ability to create a ballot paper.

--- app/controllers/ballot/ballotRegisterCtrl.js
+++ app/controllers/ballot/ballotRegisterCtrl.js
@@ -10,81 +10,17 @@ appCivistApp.controller('ballotRegisterCtrl', function($scope, $http, $routePara
     alert(data);
   });
 
-	// $scope.uuid = $routeParams.uuid;
-	// var resource = GetRegistrationForm.form($scope.uuid);
-	// $scope.returned = {
-	// 	"votingBallotRegistrationFormId":"1",
-	// 	"fields": [
-	// 		{
-	// 			"votingBallotRegistrationFieldId":1,
-	// 			"fieldName":"",
-	// 			"fieldDescription":"",
-	// 			"providedValue":""
-	// 		}
-	// 	]
-	// };
-	/*to be deleted when backend is ready*/
-	// $scope.returned = resource;
-	// $scope.fields = resource.fields;
-	/*uncomment when backend is ready!*/
-	// resource.get().$promise.then(function(data){
-	// 	$scope.returned.votingBallotRegistrationFormId = data.votingBallotRegistrationFormId;
-	// 	$scope.fields = data.fields;
-	// 	while ($scope.returned.fields.length<$scope.fields.length) {
-	// 		$scope.returned.fields[$scope.returned.fields.length] =
-	// 		{
-
-	//       		"votingBallotRegistrationFieldId":,
-	//         	"fieldName":"",
-	//             "fieldDescription":""
-
-	//    		};
-	// 	}
-	// 	for (var i=0;i<$scope.returned.fields.length;i+=1) {
-	// 		$scope.returned.fields[i].votingBallotRegistrationFieldId = $scope.fields[i].votingBallotRegistrationFieldId;
-	// 	}
-	// });
-
-	$scope.fillForm = function() {
-		for (var i=0;i<$scope.returned.fields.length;i++) {
-			$scope.returned.fields[i].providedValue = document.getElementById($scope.fields[i].votingBallotRegistrationFieldId).value;
-		}
-		/* uncomment when backend is ready */
-		// var form = resource.$save($scope.returned);
-		// form.$promise.then(
-		// 	function(data){
-		// 		// $scope.password = data.password;
-		// 		$scope.signature = data.signature;
-		// 		console.log("Posted voting registration form.");
-		// 	},
-		// 	function(error) {
-		// 		alert("Sorry, the provided information cannot generate a valid voting ballot.");
-		// 		return;
-		// 	}
-		// )
-		// var ballot = Ballot.ballot($scope.uuid, $scope.signature).get();
-		// ballot.$promise.then(
-		// 	function(data){
-		// 		$scope.votingBallot= data;
-		// localStorageService.set("currentBallot", data);
-		// 	},
-		// 	function(error){
-		// 		alert("Sorry, a valid ballot could not be obtained from the server.");
-		// 		return;
-		// 	}
-		// )
-
-		var ballot = Ballot.ballot($scope.uuid, 1);
-
-		/*save the votingBallot to the localservice so that we can access it from other controllers*/
-		localStorageService.set("currentBallot",ballot);
-		// TODO: below if is only a temporal fix to always redirect to a valid ballot url
-		var redirect = '/ballot/'+$scope.votingBallot.uuid+"/vote";
-		if (!$scope.votingBallot || !$scope.votingBallot.uuid) {
-			redirect = "/ballot/abcd-efgh-ijkl-mnop/vote";
-		}
-		$location.url(redirect);
-	}
+	$scope.createBallotPaper = function() {
+		var ballot = Ballot.save({uuid: $routeParams.uuid}, {ballot_registration_fields: $scope.ballot.ballot_registration_fields});
+		ballot.$promise.then(function(data){
+      console.log("Posted voting registration form.");
+      alert("You've successfully registered! Your unique registration signature is " + data.signature + ". Please record it to access your ballot at a later date.")
+      $location.url("/ballot/" + $routeParams.uuid + "/vote");
+		}, function(error) {
+			alert("Sorry, the provided information cannot generate a valid voting ballot.");
+			return;
+		})
+	};
 
 	$scope.loadBallot = function(){
 		/*uncomment when backend is ready*/

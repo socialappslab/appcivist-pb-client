9bd71f101a530a3e85574b1b6692dbabf1b2489c
Jorge Ramírez <jorgeramirez1990@gmail.com>
2016-11-22 20:32:01 -0300

    Sync campaign dashboard with new design
    refs #282

--- app/v2/controllers/campaign/dashboard.js
+++ app/v2/controllers/campaign/dashboard.js
@@ -1,152 +1,158 @@
-(function() {
-'use strict';
+(function () {
+  'use strict';
 
-angular
-  .module('appCivistApp')
-  .controller('v2.CampaignDashboardCtrl', CampaignDashboardCtrl);
+  angular
+    .module('appCivistApp')
+    .controller('v2.CampaignDashboardCtrl', CampaignDashboardCtrl);
 
 
-CampaignDashboardCtrl.$inject = [
-  '$scope', 'Campaigns', '$stateParams', 'Assemblies', 'Contributions', '$filter',
-  'localStorageService', 'FlashService', 'Memberships'
-];
+  CampaignDashboardCtrl.$inject = [
+    '$scope', 'Campaigns', '$stateParams', 'Assemblies', 'Contributions', '$filter',
+    'localStorageService', 'FlashService', 'Memberships'
+  ];
 
-function CampaignDashboardCtrl($scope, Campaigns, $stateParams, Assemblies, Contributions,
-                               $filter, localStorageService, FlashService, Memberships) {
+  function CampaignDashboardCtrl($scope, Campaigns, $stateParams, Assemblies, Contributions,
+    $filter, localStorageService, FlashService, Memberships) {
 
-  activate();
+    activate();
 
-  function activate() {
-    // Example http://localhost:8000/#/v2/assembly/8/campaign/56c08723-0758-4319-8dee-b752cf8004e6
-    var pattern = /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
-    $scope.isAnonymous = false;
-    if ($stateParams.cuuid && pattern.test($stateParams.cuuid) === true) {
-      $scope.campaignID = $stateParams.cuuid;
-      $scope.isAnonymous = true;
-    } else {
-      $scope.assemblyID = ($stateParams.aid) ? parseInt($stateParams.aid) : 0;
-      $scope.campaignID = ($stateParams.cid) ? parseInt($stateParams.cid) : 0;
-      $scope.user = localStorageService.get('user');
-      $scope.ideasSectionExpanded = true;
-    }
-    $scope.showResourcesSection = false;
-    $scope.toggleResourcesSection = toggleResourcesSection;
-    loadAssembly();
-    loadCampaigns();
-    loadCampaignResources();
+    function activate() {
+      // Example http://localhost:8000/#/v2/assembly/8/campaign/56c08723-0758-4319-8dee-b752cf8004e6
+      var pattern = /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
+      $scope.isAnonymous = false;
+      $scope.ideasSectionExpanded = false;
 
-	  $scope.myObject = {};
-    $scope.myObject.refreshMenu = function() {
-      $scope.myObject.showActionMenu = !$scope.myObject.showActionMenu;
-    };
-  }
-
-  function loadAssembly() {
-    var rsp = Assemblies.assembly($scope.assemblyID).get();
-    rsp.$promise.then(function(data) {
-      $scope.assembly = data;
-    });
-  }
-
-  function loadCampaigns() {
-    var res;
-    if ($scope.isAnonymous) {
-      res = Campaigns.campaignByUUID($scope.campaignID).get();
-    } else {
-      res = Campaigns.campaign($scope.assemblyID, $scope.campaignID).get();
-    }
-
-    res.$promise.then(function(data) {
-      $scope.campaign = data;
-      if(!$scope.isAnonymous) {
-        $scope.spaceID = data.resourceSpaceId;
-      }else{
-        $scope.spaceID = data.resourceSpaceUUId;
+      if ($stateParams.cuuid && pattern.test($stateParams.cuuid) === true) {
+        $scope.campaignID = $stateParams.cuuid;
+        $scope.isAnonymous = true;
+      } else {
+        $scope.assemblyID = ($stateParams.aid) ? parseInt($stateParams.aid) : 0;
+        $scope.campaignID = ($stateParams.cid) ? parseInt($stateParams.cid) : 0;
+        $scope.user = localStorageService.get('user');
       }
-      var currentComponent = Campaigns.getCurrentComponent(data.components);
-      setIdeasSectionVisibility(currentComponent);
+      $scope.showResourcesSection = false;
+      $scope.toggleResourcesSection = toggleResourcesSection;
+      $scope.toggleIdeasSection = toggleIdeasSection;
+      loadAssembly();
+      loadCampaigns();
+      loadCampaignResources();
 
-      // get proposals
-      getContributions($scope.campaign, 'PROPOSAL').then(function(response) {
-        // only published proposals
-        $scope.proposals = response.splice(0, 6);
-
-        if(!$scope.proposals){
-          $scope.proposals = [];
-        }
-      });
-
-      // get ideas
-      getContributions($scope.campaign, 'IDEA').then(function(response) {
-        $scope.ideas = response.splice(0, 6);
-
-        if(!$scope.ideas){
-          $scope.ideas = [];
-        }
-      });
-
-      // get discussions
-      getContributions($scope.campaign, 'DISCUSSION').then(function(response) {
-        $scope.discussions = response;
-
-        if(!$scope.discussions){
-          $scope.discussions = [];
-        }
-      });
-    });
-  }
-
-  function setIdeasSectionVisibility(component) {
-    var key = component.key.toUpperCase();
-    $scope.isIdeasSectionVisible = (key === 'PROPOSAL MAKING' || key === 'IDEAS');
-  }
-
-  /**
-   * Get contributions from server.
-   *
-   * @param campaign {Campaign} the current campaign.
-   * @param type {String} forum_post | comment | idea | question | issue |  proposal | note
-   * @return promise
-   **/
-  function getContributions(campaign, type) {
-    // Get list of contributions from server
-    var rsp;
-    var query = {type: type};
-    
-    if(type === 'IDEA' || type === 'PROPOSAL'){
-      query.sort = 'date';
+      $scope.myObject = {};
+      $scope.myObject.refreshMenu = function () {
+        $scope.myObject.showActionMenu = !$scope.myObject.showActionMenu;
+      };
     }
-    
-    if ($scope.isAnonymous){
-      rsp = Contributions.contributionInResourceSpaceByUUID(campaign.resourceSpaceUUId).query(query);
-    }else{
-      rsp = Contributions.contributionInResourceSpace(campaign.resourceSpaceId).query(query);
+
+    function loadAssembly() {
+      var rsp = Assemblies.assembly($scope.assemblyID).get();
+      rsp.$promise.then(function (data) {
+        $scope.assembly = data;
+      });
     }
-    rsp.$promise.then(
-      function (data) {
-        return data;
-      },
-      function (error) {
-        FlashService.Error('Error loading campaign contributions from server');
-      }
-    );
-    return rsp.$promise;
-  }
 
-  function loadCampaignResources() {
-    var rsp = Campaigns.resources($scope.assemblyID, $scope.campaignID).query();
-    rsp.$promise.then(
-      function(resources) {
-        $scope.campaignResources = resources;
-      },
-      function(error) {
-        FlashService.Error('Error loading campaign resources from server');
+    function loadCampaigns() {
+      var res;
+      if ($scope.isAnonymous) {
+        res = Campaigns.campaignByUUID($scope.campaignID).get();
+      } else {
+        res = Campaigns.campaign($scope.assemblyID, $scope.campaignID).get();
       }
-    );
-  }
 
-  function toggleResourcesSection() {
-    $scope.showResourcesSection = !$scope.showResourcesSection;
+      res.$promise.then(function (data) {
+        $scope.campaign = data;
+        if (!$scope.isAnonymous) {
+          $scope.spaceID = data.resourceSpaceId;
+        } else {
+          $scope.spaceID = data.resourceSpaceUUId;
+        }
+        var currentComponent = Campaigns.getCurrentComponent(data.components);
+        setIdeasSectionVisibility(currentComponent);
+
+        // get proposals
+        getContributions($scope.campaign, 'PROPOSAL').then(function (response) {
+          // only published proposals
+          $scope.proposals = response.splice(0, 6);
+
+          if (!$scope.proposals) {
+            $scope.proposals = [];
+          }
+        });
+
+        // get ideas
+        getContributions($scope.campaign, 'IDEA').then(function (response) {
+          $scope.ideas = response.splice(0, 6);
+
+          if (!$scope.ideas) {
+            $scope.ideas = [];
+          }
+        });
+
+        // get discussions
+        getContributions($scope.campaign, 'DISCUSSION').then(function (response) {
+          $scope.discussions = response;
+
+          if (!$scope.discussions) {
+            $scope.discussions = [];
+          }
+        });
+      });
+    }
+
+    function setIdeasSectionVisibility(component) {
+      var key = component.key.toUpperCase();
+      $scope.isIdeasSectionVisible = (key === 'PROPOSAL MAKING' || key === 'IDEAS');
+    }
+
+    /**
+     * Get contributions from server.
+     *
+     * @param campaign {Campaign} the current campaign.
+     * @param type {String} forum_post | comment | idea | question | issue |  proposal | note
+     * @return promise
+     **/
+    function getContributions(campaign, type) {
+      // Get list of contributions from server
+      var rsp;
+      var query = { type: type };
+
+      if (type === 'IDEA' || type === 'PROPOSAL') {
+        query.sort = 'date';
+      }
+
+      if ($scope.isAnonymous) {
+        rsp = Contributions.contributionInResourceSpaceByUUID(campaign.resourceSpaceUUId).query(query);
+      } else {
+        rsp = Contributions.contributionInResourceSpace(campaign.resourceSpaceId).query(query);
+      }
+      rsp.$promise.then(
+        function (data) {
+          return data;
+        },
+        function (error) {
+          FlashService.Error('Error loading campaign contributions from server');
+        }
+      );
+      return rsp.$promise;
+    }
+
+    function loadCampaignResources() {
+      var rsp = Campaigns.resources($scope.assemblyID, $scope.campaignID).query();
+      rsp.$promise.then(
+        function (resources) {
+          $scope.campaignResources = resources;
+        },
+        function (error) {
+          FlashService.Error('Error loading campaign resources from server');
+        }
+      );
+    }
+
+    function toggleResourcesSection() {
+      $scope.showResourcesSection = !$scope.showResourcesSection;
+    }
+
+    function toggleIdeasSection() {
+      $scope.ideasSectionExpanded = !$scope.ideasSectionExpanded;
+    }
   }
-}
-}());
+} ());

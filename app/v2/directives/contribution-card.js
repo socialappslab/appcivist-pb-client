f0fe16b75fa09c27d9e3bb812be8ee8fbf3a0045
Jorge Ramírez <jorgeramirez1990@gmail.com>
2016-11-22 21:09:13 -0300

    Show/hide contextual menu in contribution cards
    refs #282

--- app/v2/directives/contribution-card.js
+++ app/v2/directives/contribution-card.js
@@ -1,102 +1,110 @@
-(function() {
-'use strict';
+(function () {
+  'use strict';
 
-appCivistApp
-  .directive('contributionCard',  ContributionCard);
+  appCivistApp
+    .directive('contributionCard', ContributionCard);
 
-ContributionCard.$inject = ['Contributions', 'Campaigns', 'localStorageService', 'Memberships', '$window'];
+  ContributionCard.$inject = ['Contributions', 'Campaigns', 'localStorageService', 'Memberships', '$window'];
 
-function ContributionCard(Contributions, Campaigns, localStorageService, Memberships, $window) {
+  function ContributionCard(Contributions, Campaigns, localStorageService, Memberships, $window) {
 
-  function hasRole(roles, roleName) {
-    var result = false;
+    function hasRole(roles, roleName) {
+      var result = false;
 
-    angular.forEach(roles, function(role){
-      if(role.name === roleName) {
-        result = true;
-      }
-    });
-    return result;
-  }
-
-  function setupMembershipInfo(scope) {
-    scope.userIsAuthor = Contributions.verifyAuthorship(scope.user, scope.contribution);
-
-    var authorship = Contributions.verifyGroupAuthorship(scope.user, scope.contribution, scope.group).get();
-    authorship.$promise.then(function(response){
-      scope.userCanEdit = response.responseStatus === 'OK';
-    });
-
-    var rsp = Memberships.membershipInAssembly(scope.assemblyId, scope.user.userId).get();
-    rsp.$promise.then(function(data) {
-      scope.userIsAssemblyCoordinator = hasRole(data.roles, 'COORDINATOR');
-    });
-
-    rsp = Memberships.membershipInGroup(scope.groupId, scope.user.userId).get();
-    rsp.$promise.then(function(data) {
-      scope.userIsWorkingGroupCoordinator = hasRole(data.roles, 'COORDINATOR');
-    });
-  }
-
-  function setContributionType(scope) {
-    scope.isProposal = scope.contribution.type === 'PROPOSAL';
-    scope.isIdea = scope.contribution.type === 'IDEA';
-  }
-
-  return {
-    restrict: 'E',
-    scope: {
-      contribution: '=',
-      showVotingButtons: '=',
-      campaign: '='
-    },
-    templateUrl: '/app/v2/partials/directives/contribution-card.html',
-    link: function postLink(scope, element, attrs) {
-      scope.user = localStorageService.get('user');
-      scope.isAnonymous = !scope.user;
-      setContributionType(scope);
-
-      if(!scope.isIdea) {
-        var workingGroupAuthors = scope.contribution.workingGroupAuthors;
-        var workingGroupAuthorsLength = workingGroupAuthors ? workingGroupAuthors.length : 0;
-        scope.group = workingGroupAuthorsLength ? workingGroupAuthors[0] : 0;
-
-        if(!scope.isAnonymous) {
-          scope.groupId = workingGroupAuthorsLength ? scope.contribution.workingGroupAuthors[0].groupId : 0;
-          scope.assemblyId = localStorageService.get('currentAssembly').assemblyId;
+      angular.forEach(roles, function (role) {
+        if (role.name === roleName) {
+          result = true;
         }
-      }
+      });
+      return result;
+    }
 
-      if(scope.campaign) {
-        // Verify the status of the campaign and show or not show the voting buttons
-        var currentComponent = Campaigns.getCurrentComponent(scope.campaign.components);
-        if (currentComponent.key === 'Voting') {
-          scope.showVotingButtons = true;
+    function setupMembershipInfo(scope) {
+      scope.userIsAuthor = Contributions.verifyAuthorship(scope.user, scope.contribution);
+
+      var authorship = Contributions.verifyGroupAuthorship(scope.user, scope.contribution, scope.group).get();
+      authorship.$promise.then(function (response) {
+        scope.userCanEdit = response.responseStatus === 'OK';
+      });
+
+      var rsp = Memberships.membershipInAssembly(scope.assemblyId, scope.user.userId).get();
+      rsp.$promise.then(function (data) {
+        scope.userIsAssemblyCoordinator = hasRole(data.roles, 'COORDINATOR');
+      });
+
+      rsp = Memberships.membershipInGroup(scope.groupId, scope.user.userId).get();
+      rsp.$promise.then(function (data) {
+        scope.userIsWorkingGroupCoordinator = hasRole(data.roles, 'COORDINATOR');
+      });
+    }
+
+    function setContributionType(scope) {
+      scope.isProposal = scope.contribution.type === 'PROPOSAL';
+      scope.isIdea = scope.contribution.type === 'IDEA';
+    }
+
+    function toggleContextualMenu() {
+      this.showContextualMenu = !this.showContextualMenu;
+      console.log('holaa', this.showContextualMenu);
+
+    }
+
+    return {
+      restrict: 'E',
+      scope: {
+        contribution: '=',
+        showVotingButtons: '=',
+        campaign: '='
+      },
+      templateUrl: '/app/v2/partials/directives/contribution-card.html',
+      link: function postLink(scope, element, attrs) {
+        scope.user = localStorageService.get('user');
+        scope.isAnonymous = !scope.user;
+        scope.showContextualMenu = false;
+        scope.toggleContextualMenu = toggleContextualMenu.bind(scope);
+        setContributionType(scope);
+
+        if (!scope.isIdea) {
+          var workingGroupAuthors = scope.contribution.workingGroupAuthors;
+          var workingGroupAuthorsLength = workingGroupAuthors ? workingGroupAuthors.length : 0;
+          scope.group = workingGroupAuthorsLength ? workingGroupAuthors[0] : 0;
+
+          if (!scope.isAnonymous) {
+            scope.groupId = workingGroupAuthorsLength ? scope.contribution.workingGroupAuthors[0].groupId : 0;
+            scope.assemblyId = localStorageService.get('currentAssembly').assemblyId;
+          }
         }
-      }
-      scope.showActionMenu = false;
-      scope.myObject = {};
-      scope.myObject.refreshMenu = function() {
-        scope.showActionMenu = !scope.showActionMenu;
-      };
 
-      // Read user contribution feedback
-      if (scope.userFeedback === undefined || scope.userFeedback === null) {
-        scope.userFeedback = {'up':false, 'down':false, 'fav': false, 'flag': false};
-      }
+        if (scope.campaign) {
+          // Verify the status of the campaign and show or not show the voting buttons
+          var currentComponent = Campaigns.getCurrentComponent(scope.campaign.components);
+          if (currentComponent.key === 'Voting') {
+            scope.showVotingButtons = true;
+          }
+        }
+        scope.showActionMenu = false;
+        scope.myObject = {};
+        scope.myObject.refreshMenu = function () {
+          scope.showActionMenu = !scope.showActionMenu;
+        };
 
-      // Feedback update
-      scope.myObject.updateFeedback = function (value) {
+        // Read user contribution feedback
+        if (scope.userFeedback === undefined || scope.userFeedback === null) {
+          scope.userFeedback = { 'up': false, 'down': false, 'fav': false, 'flag': false };
+        }
+
+        // Feedback update
+        scope.myObject.updateFeedback = function (value) {
           if (value === 'up') {
-              scope.userFeedback.up = true;
-              scope.userFeedback.down = false;
+            scope.userFeedback.up = true;
+            scope.userFeedback.down = false;
           } else if (value === 'down') {
-              scope.userFeedback.up = false;
-              scope.userFeedback.down = true;
+            scope.userFeedback.up = false;
+            scope.userFeedback.down = true;
           } else if (value === 'fav') {
-              scope.userFeedback.fav = true;
+            scope.userFeedback.fav = true;
           } else if (value === 'flag') {
-              scope.userFeedback.flag = true;
+            scope.userFeedback.flag = true;
           } else if (value === undefined) {
             if (scope.userFeedback.up == scope.userFeedback.down) {
               scope.userFeedback.up = true;
@@ -110,38 +118,38 @@ function ContributionCard(Contributions, Campaigns, localStorageService, Members
           //var stats = scope.contribution.stats;
           var feedback = Contributions.userFeedback(scope.assemblyId, scope.contribution.contributionId).update(scope.userFeedback);
           feedback.$promise.then(
-              function (newStats) {
-                  scope.contribution.stats = newStats;
-              },
-              function (error) {
-                  console.log('Error when updating user feedback');
-              }
+            function (newStats) {
+              scope.contribution.stats = newStats;
+            },
+            function (error) {
+              console.log('Error when updating user feedback');
+            }
           );
-      };
+        };
 
-      //change redirection
-      scope.myObject.softRemoval = function() {
-        Contributions.contributionSoftRemoval(scope.assemblyId, scope.contribution.contributionId).update(scope.contribution);
-        $window.location.reload();
+        //change redirection
+        scope.myObject.softRemoval = function () {
+          Contributions.contributionSoftRemoval(scope.assemblyId, scope.contribution.contributionId).update(scope.contribution);
+          $window.location.reload();
+        }
+
+        scope.myObject.publish = function () {
+          Contributions.publishContribution(scope.assemblyId, scope.contribution.contributionId).update(scope.contribution);
+          $window.location.reload();
+        }
+
+        scope.myObject.exclude = function () {
+          Contributions.excludeContribution(scope.assemblyId, scope.contribution.contributionId).update(scope.contribution);
+          $window.location.reload();
+        }
+
+        //find endpoint
+        scope.myObject.assignToWG = function () {
+          //Contributions.assignContributionToWG(scope.assemblyId, scope.contribution.contributionId, scope.wg).update(scope.contribution);
+          $window.location.reload();
+        }
+
       }
-
-      scope.myObject.publish = function() {
-        Contributions.publishContribution(scope.assemblyId, scope.contribution.contributionId).update(scope.contribution);
-        $window.location.reload();
-      }
-
-      scope.myObject.exclude = function() {
-        Contributions.excludeContribution(scope.assemblyId, scope.contribution.contributionId).update(scope.contribution);
-        $window.location.reload();
-      }
-
-      //find endpoint
-      scope.myObject.assignToWG = function() {
-        //Contributions.assignContributionToWG(scope.assemblyId, scope.contribution.contributionId, scope.wg).update(scope.contribution);
-        $window.location.reload();
-      }
-
-    }
-  };
-}
-}());
+    };
+  }
+} ());

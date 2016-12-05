503c84de01ff469512b865445a94d5631f393846
Jorge Ramírez <jorgeramirez1990@gmail.com>
2016-12-05 11:31:44 -0300

    Add milestones contextual menu in campaign status timeline

--- app/v2/directives/campaign-timeline.js
+++ app/v2/directives/campaign-timeline.js
@@ -1,68 +1,81 @@
-(function() {
-'use strict';
+(function () {
+  'use strict';
 
-appCivistApp
-  .directive('campaignTimeline',  CampaignTimeline);
+  appCivistApp
+    .directive('campaignTimeline', CampaignTimeline);
 
-CampaignTimeline.$inject = ['Campaigns', 'localStorageService'];
+  CampaignTimeline.$inject = ['Campaigns', 'localStorageService'];
 
-function CampaignTimeline(Campaigns, localStorageService) {
+  function CampaignTimeline(Campaigns, localStorageService) {
 
-  function loadCampaign(scope, aid, cid) {
-    scope.user = localStorageService.get('user');
-    var res;
-    if (scope.user) {
-      res = Campaigns.campaign(aid, cid).get();
-    } else {
-      res = Campaigns.campaignByUUID(cid).get();
-    }
-    res.$promise.then(function(data) {
-      var currentComponent = Campaigns.getCurrentComponent(data.components);
-      angular.forEach(data.components, function(c) {
-        c.cssClass = getComponentCssClass(scope, currentComponent, c);
-      });
-      scope.components = data.components;
-    });
-  }
-
-  /**
-   * Set timeline stage status.
-   *
-   * @param currentComponent {Component} the current component.
-   * @param c {Component} the timeline component.
-   **/
-  function getComponentCssClass(scope, currentComponent, c) {
-    var idField = scope.user ? 'componentId' : 'uuid';
-
-    if(c[idField] === currentComponent[idField]) {
-      return 'active';
-    }
-
-    if(c.position < currentComponent.position) {
-      return 'inactive';
-    }
-    return 'future';
-  }
-
-  return {
-    restrict: 'E',
-    scope: {
-      assemblyId: '=',
-      campaignId: '=',
-      title: '@',
-      onlyLabel: '='
-    },
-    templateUrl: '/app/v2/partials/directives/campaign-timeline.html',
-    link: function postLink(scope, element, attrs) {
-
-      if(!scope.campaignId){
-        scope.$watch('campaignId', function(cid) {
-          loadCampaign(scope, scope.assemblyId, cid);
-        });
-      }else{
-        loadCampaign(scope, scope.assemblyId, scope.campaignId);
+    function loadCampaign(scope, aid, cid) {
+      scope.user = localStorageService.get('user');
+      var res;
+      if (scope.user) {
+        res = Campaigns.campaign(aid, cid).get();
+      } else {
+        res = Campaigns.campaignByUUID(cid).get();
       }
+      res.$promise.then(function (data) {
+        var currentComponent = Campaigns.getCurrentComponent(data.components);
+        angular.forEach(data.components, function (c) {
+          c.cssClass = getComponentCssClass(scope, currentComponent, c);
+        });
+        scope.components = data.components;
+      });
     }
-  };
-}
-}());
+
+    /**
+     * Set timeline stage status.
+     *
+     * @param currentComponent {Component} the current component.
+     * @param c {Component} the timeline component.
+     **/
+    function getComponentCssClass(scope, currentComponent, c) {
+      var idField = scope.user ? 'componentId' : 'uuid';
+
+      if (c[idField] === currentComponent[idField]) {
+        return 'active';
+      }
+
+      if (c.position < currentComponent.position) {
+        return 'inactive';
+      }
+      return 'future';
+    }
+
+    return {
+      restrict: 'E',
+      scope: {
+        assemblyId: '=',
+        campaignId: '=',
+        title: '@',
+        onlyLabel: '='
+      },
+      templateUrl: '/app/v2/partials/directives/campaign-timeline.html',
+      link: function postLink(scope, element, attrs) {
+
+        if (!scope.campaignId) {
+          scope.$watch('campaignId', function (cid) {
+            loadCampaign(scope, scope.assemblyId, cid);
+          });
+        } else {
+          loadCampaign(scope, scope.assemblyId, scope.campaignId);
+        }
+
+        scope.formatDate = function (date) {
+          return moment(date, 'YYYY-MM-DD HH:mm').local().format('L');
+        };
+
+        scope.toggleMilestoneDescription = function (milestone, milestones) {
+          angular.forEach(milestones, function (m) {
+            if (milestone.componentMilestoneId !== m.componentMilestoneId) {
+              m.showDescription = false;
+            }
+          });
+          milestone.showDescription = !milestone.showDescription;
+        }
+      }
+    };
+  }
+} ());

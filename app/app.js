5b62f59b7242803aa4dd400aa930c0e62f6ba48a
Cristhian Parra <cdparra@gmail.com>
2016-12-10 04:09:06 -0800

    Tried to integrate the new pattern styles and its treatment of resizing cards, but still not working

--- app/app.js
+++ app/app.js
@@ -408,7 +408,7 @@
    */
   run.$inject = [
     '$rootScope', '$location', '$http', 'localStorageService', 'logService', '$uibModal',
-    'usSpinnerService', '$timeout'
+    'usSpinnerService', '$timeout', '$document'
   ];
 
   /**
@@ -419,7 +419,7 @@
    * @param localStorageService
    */
   function run($rootScope, $location, $http, localStorageService, logService, $uibModal, usSpinnerService,
-    $timeout) {
+    $timeout, $document) {
     localStorageService.set("serverBaseUrl", appCivistCoreBaseURL);
     localStorageService.set("votingApiUrl", votingApiUrl);
     localStorageService.set("etherpadServer", etherpadServerURL);
@@ -525,6 +525,49 @@
       usSpinnerService.stop(key);
       $(angular.element.find(element)[0]).removeClass('spinner-container');
     };
+
+    document.addEventListener('resize', $rootScope.onResize);
+
+    document.addEventListener('eqResize', function(){
+      $rootScope.onResize();
+    }.bind(this));
+
+    $rootScope.onResize = function () {
+      if(document.querySelector('.container__proposals') != null) {
+        EqualHeights('.container__proposals .heading__working_group');
+        EqualHeights('.container__proposals .heading--headline');
+        EqualHeights('.container__proposals .title_block');
+        EqualHeights('.container__proposals .card__header');
+        EqualHeights('.container__proposals .card__body .excerpt');
+        EqualHeights('.container__proposals .card__body');
+      }
+
+      if(document.querySelector('.container__ideas') != null) {
+        EqualHeights('.container__ideas .card__header .heading--headline');
+        EqualHeights('.container__ideas .card__header');
+      }
+    }
+
+    /**
+     * Added from pattern library appcivist_ui_core.js
+     * Fixes some issues with contribution cards
+     */
+    function EqualHeights (selector) {
+      var elms = document.querySelectorAll(selector);
+      var len = elms.length;
+      var tallest = 0;
+      var elm, elmHeight, x;
+
+      for (x=0; x < len; x++) {
+        elm = elms[x];
+        elmHeight = elm.offsetHeight;
+        tallest = (elmHeight > tallest) ? elmHeight : tallest;
+      }
+
+      for (x=0; x < len; x++) {
+        elms[x].style.height = tallest + 'px';
+      }
+    }
   }
 
   /**

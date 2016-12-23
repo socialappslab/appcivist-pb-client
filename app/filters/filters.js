ec6f2268f6f086ecd0ca1d9e4c82921e18f812d7
Jorge Ramírez <jorgeramirez1990@gmail.com>
2016-12-23 19:38:44 -0300

    History modal for contributions.

--- app/filters/filters.js
+++ app/filters/filters.js
@@ -39,3 +39,12 @@ appCivistApp.filter('startFrom', function() {
         return input.slice(start);
     }
 });
+
+appCivistApp.filter('titlecase', function() {
+    return function(s) {
+        s = ( s === undefined || s === null ) ? '' : s;
+        return s.toString().toLowerCase().replace( /\b([a-z])/g, function(ch) {
+            return ch.toUpperCase();
+        });
+    };
+});

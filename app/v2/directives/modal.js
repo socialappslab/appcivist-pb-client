4f685da79b9289a7633f40054e650e6d79fa76c2
Cristhian Parra <cdparra@gmail.com>
2017-01-28 00:33:37 -0300

    Fixed strange bug with vex modals. Theme can be set only through defaultOptions

--- app/v2/directives/modal.js
+++ app/v2/directives/modal.js
@@ -3,11 +3,11 @@
 
   /**
    * Very simple modal directive.
-   * 
+   *
    * Usage:
-   * 
+   *
    * <modal title="Title" open-if="isModalOpened" close="closeModal('ID')" template-id="ID"></modal>
-   * 
+   *
    * <script id="ID" type="text/ng-template">
    *  // modal body
    * </script>
@@ -28,6 +28,7 @@
       templateUrl: '/app/v2/partials/directives/modal.html',
       link: function(scope, element, attrs) {
         var vexInstance;
+        vex.defaultOptions.className = 'vex-theme-plain';
 
         scope.$watch('openIf', function(open) {
           if (vexInstance) {
@@ -39,7 +40,6 @@
           }
           var html = document.getElementById(scope.templateId).innerHTML;
           vexInstance = vex.open({
-            className: 'vex-theme-plain',
             unsafeContent: $compile(html)(scope.$parent)[0],
             afterClose: function() {
               if (angular.isFunction(scope.close)) {

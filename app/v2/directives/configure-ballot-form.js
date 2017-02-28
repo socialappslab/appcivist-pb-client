7137f4baa555a926fa9a66b148646b9bf7ea9422
Jorge Ramírez <jorgeramirez1990@gmail.com>
2017-02-28 15:01:39 -0300

    Connect ballot configuration form to voting API
    refs #354

--- app/v2/directives/configure-ballot-form.js
+++ app/v2/directives/configure-ballot-form.js
@@ -10,13 +10,13 @@
    * 
    * @example
    * 
-   * <configure-ballot-form></configure-ballot-form>
+   * <configure-ballot-form close="vm.closeModal('configureBallotForm')"></configure-ballot-form>
    */
   appCivistApp
     .component('configureBallotForm', {
       selector: 'configureBallotForm',
       bindings: {
-
+        close: '&?'
       },
       controller: FormCtrl,
       controllerAs: 'vm',
@@ -24,11 +24,12 @@
     });
 
   FormCtrl.$inject = [
-    'Notify', 'Components'
+    'Notify', 'Components', 'Ballot'
   ];
 
-  function FormCtrl(Notify, Components) {
+  function FormCtrl(Notify, Components, Ballot) {
     var vm = this;
+    Object.assign(this, { Notify: Notify, Ballot: Ballot });
 
     this.$onInit = function() {
       vm.model = {
@@ -50,14 +51,61 @@
    */
   function onSelect() {
     var vm = this;
-    this.selectedConfigs = this.configs.filter(function(c) { console.log(c); return c.dependsOfValue === vm.model.selected.value });
+    this.selectedConfigs = this.configs.filter(function(c) { return c.dependsOfValue === vm.model.selected.value });
   }
 
   /**
    * Submits the form
    */
   function submit() {
-    // TODO: implement creation logic
-    console.log(this.model);
+    var now = moment();
+    var dateFormat = 'YYYY-MM-DD hh:mm:ss';
+
+    var data = {
+      ballot: {
+        'starts_at': now.format(dateFormat),
+        'ends_at': now.add(1, 'M').format(dateFormat),
+        'notes': 'ballot notes',
+        'instructions': 'ballot instructions'
+      },
+      'ballot_configurations': [],
+      'ballot_registration_fields': [{
+        'name': 'Name',
+        'description': 'Enter a name or an identifier',
+        'expected_value': 'String'
+      }]
+    };
+    var vm = this;
+    // set default values for ballot configurations
+    angular.forEach(vm.selectedConfigs, function(config) {
+      data['ballot_configurations'].push({
+        key: config.key,
+        value: config.value
+      });
+    });
+    // ballot password
+    var ballotPassword = _.find(vm.configs, { key: 'component.voting.ballot.password' });
+    data.password = ballotPassword.value;
+    data.ballot['voting_system_type'] = this.model.selected.value;
+    // override default ballot configuration values
+    angular.forEach(this.model.configurations, function(value, configPosition) {
+      var config = _.find(vm.selectedConfigs, { position: parseInt(configPosition) });
+      var ballotConfig = _.find(data['ballot_configurations'], { key: config.key });
+      ballotConfig.value = value;
+    });
+
+    var rsp = this.Ballot.create(data).$promise;
+    rsp.then(
+      function(response) {
+        vm.Notify.show('Voting ballot created successfully', 'success');
+
+        if (angular.isFunction(vm.close)) {
+          vm.close();
+        }
+      },
+      function(error) {
+        vm.Notify.show(error.data.error, 'error');
+      }
+    );
   }
 }())

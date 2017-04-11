db466cc9a6b3d937b0742d4a486b3c4d0baa4756
Jorge Ramírez <jorgeramirez1990@gmail.com>
2017-04-11 15:22:15 -0400

    Update proposals list after creating a new one
    - Refactor contribution-form directive to use controllerAs property.
    - Add event pagination:reloadCurrentPage to pagination-widget
    
    refs #582

--- app/v2/directives/pagination-widget.js
+++ app/v2/directives/pagination-widget.js
@@ -1,6 +1,22 @@
-(function () {
+(function() {
   'use strict';
 
+  /**
+   * @name paginationWidget
+   * @memberof directives
+   * 
+   * @description
+   * 
+   * Implements a pagination widget for contributions. The widget also setup a listener for the event
+   * <i>pagination:reloadCurrentPage</i> in order to reload the current page.
+   * 
+   *
+   * @example 
+   * 
+   * <pagination-widget page-size="pageSize" space="spaceID" resource="contribution" type="type" is-anonymous="isAnonymous"
+   *                    is-coordinator="isCoordinator" sorting="sorting" ng-if="showPagination"></pagination-widget>
+   * 
+   */
   angular
     .module('appCivistApp')
     .directive('paginationWidget', paginationWidget);
@@ -9,10 +25,8 @@
 
   function paginationWidget($state, Contributions, Notify) {
     var directive = {
-      //required: '^^wizard',
       restrict: 'E',
       scope: {
-        //page: '=',
         pageSize: '=',
         space: '=',
         resource: '=',
@@ -25,51 +39,56 @@
       link: function postLink(scope) {
         var vm = this;
 
-        scope.$watch("sorting", function (newValue, oldValue) {
-            if (!newValue || angular.equals(newValue, oldValue)) {
-               return;
-            }
-               getResultsPage(1);
-            });
+        scope.$watch('sorting', function(newValue, oldValue) {
+          if (!newValue || angular.equals(newValue, oldValue)) {
+            return;
+          }
+          getResultsPage(1);
+        });
+
+        scope.$on('pagination:reloadCurrentPage', () => {
+          getResultsPage(scope.pagination.current);
+        });
 
         getResultsPage(1);
         scope.pagination = {
-            current: 1
+          current: 1
         };
 
         scope.pageChanged = function(newPage) {
-            getResultsPage(newPage);
+          getResultsPage(newPage);
         };
 
         function getResultsPage(pageNumber) {
-            var rsp;
-            var query = { type: scope.type.toUpperCase(), "sorting": scope.sorting };
-            if (scope.isAnonymous) {
-              rsp = Contributions.contributionInResourceSpaceByUUID(scope.space, pageNumber, scope.pageSize).get(query);
-            } else {
-              rsp = Contributions.contributionInResourceSpace(scope.space, pageNumber, scope.pageSize).get(query);
-            }
-            rsp.$promise.then(
-              function (data) {
-                var contributions = data.list;
+          var rsp;
+          var query = { type: scope.type.toUpperCase(), 'sorting': scope.sorting };
 
-                if (!contributions) {
-                  contributions = [];
-                }
-                scope.contributions = contributions;
-                scope.totalContributions = data.total; //get from response
-                scope.pagination.current = pageNumber;
-              },
-              function (error) {
-                Notify.show('Error loading proposals from server', 'error');
+          if (scope.isAnonymous) {
+            rsp = Contributions.contributionInResourceSpaceByUUID(scope.space, pageNumber, scope.pageSize).get(query);
+          } else {
+            rsp = Contributions.contributionInResourceSpace(scope.space, pageNumber, scope.pageSize).get(query);
+          }
+          rsp.$promise.then(
+            function(data) {
+              var contributions = data.list;
+
+              if (!contributions) {
+                contributions = [];
               }
-            );
+              scope.contributions = contributions;
+              scope.totalContributions = data.total; //get from response
+              scope.pagination.current = pageNumber;
+            },
+            function(error) {
+              Notify.show('Error loading proposals from server', 'error');
+            }
+          );
         }
 
         scope.paginationTop = {};
         scope.paginationBottom = {};
 
-        scope.paginationVisible = function (pag, visible) {
+        scope.paginationVisible = function(pag, visible) {
           if (scope.paginationTop.visible) {
             scope.paginationBottom.style = { display: 'none' };
             return;
@@ -80,8 +99,6 @@
 
       }
     };
-
     return directive;
   }
-
-} ());
+}());

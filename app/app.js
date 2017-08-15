13b5ba2e01b59a85ef451547dec4e7ab105b9ff1
Jorge Ramírez <jorgeramirez1990@gmail.com>
2017-08-14 22:10:15 -0400

    Minor fix
    refs socialappslab/appcivist-patterns#122

--- app/app.js
+++ app/app.js
@@ -294,7 +294,7 @@
       .state('v2.assembly.aid.campaign.cid', {
         url: '/:cid',
         controller: 'v2.CampaignDashboardCtrl',
-        templateUrl: 'app/v2/mockups/dashboard.html',
+        templateUrl: 'app/v2/partials/campaign/dashboard.html',
         /* WARNING: THIS IS JUST TO TEST THE LOWER TOOLBAR
         templateUrl: 'app/v2/mockups/dashboard.html',
         END WARNING */

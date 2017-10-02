60d5de19cc6febc0bcfedd911ae597ba67cbb7b0
Cristhian Parra <cdparra@gmail.com>
2017-10-02 06:09:21 -0300

    Updated contribution voting control to connect it to the values in the ballot paper

--- app/v2/components/contribution-card-voting-control/contribution-card-voting-control.js
+++ app/v2/components/contribution-card-voting-control/contribution-card-voting-control.js
@@ -1,3 +1,5 @@
+'use strict';
+
 (function () {
   'use strict';
 
@@ -12,20 +14,69 @@
    *
    *  <contribution-card-voting-control></contribution-card-voting-control>
    */
-  appCivistApp
-    .component('contributionCardVotingControl', {
-      selector: 'votingControl',
-      bindings: {
-        type: '@'
-      },
-      controller: contributionCardVotingControlCtrl,
-      controllerAs: 'vm',
-      templateUrl: '/app/v2/components/contribution-card-voting-control/contribution-card-voting-control.html'
-    });
 
-  contributionCardVotingControlCtrl.$inject = [
-  ];
+  appCivistApp.component('contributionCardVotingControl', {
+    selector: 'votingControl',
+    bindings: {
+      type: '@',
+      ballotPaper: '=',
+      contribution: '=',
+      ballotTokens: '='
 
-  function contributionCardVotingControlCtrl() {
+    },
+    controller: contributionCardVotingControlCtrl,
+    controllerAs: 'vm',
+    templateUrl: '/app/v2/components/contribution-card-voting-control/contribution-card-voting-control.html'
+  });
+
+  contributionCardVotingControlCtrl.$inject = ['$scope'];
+
+  function contributionCardVotingControlCtrl($scope) {
+    this.activate = activate.bind(this);
+    this.updateVote = updateVote.bind(this);
+    this.voteValue = 0;
+    /**
+     * Initialization method.
+     */
+    this.$onInit = () => {
+      $scope.$watch('vm.ballotPaper', ballotPaper => {
+        if (!ballotPaper) {
+          return;
+        }
+        this.activate();
+      });
+    };
+
+    function activate() {
+      if (this.ballotPaper) {
+        this.ballot = this.ballotPaper.ballot; // the voting ballot, which holds voting configs
+        this.voteRecord = this.ballotPaper.vote; // the ballot paper, which holds the votes of the user
+        this.candidatesIndex = this.ballot ? this.ballot.candidatesIndex : null; // map of [:contribution_uuid] => pos. of candidate in candidates array
+        this.candidates = this.ballot ? this.ballot.candidates : []; // candidates array, which holds the candidateId for each candidate
+        this.votesIndex = this.voteRecord ? this.voteRecord.votesIndex : null; // map of [:candidateId] => pos. of vote in votes array
+        this.votes = this.voteRecord ? this.voteRecord.votes : []; // array of votes, which contains the value for each vote
+        this.contributionUuid = this.contribution ? this.contribution.uuid : null; // contribution uuid for locating the candidate
+        this.candidateIndex = this.candidatesIndex && this.contributionUuid ? this.candidatesIndex[this.contributionUuid] : null; // individual candidate index
+        this.candidate = this.candidateIndex ? this.candidates[this.candidateIndex] : null; // candidate object
+        this.candidateId = this.candidate ? this.candidate.id : null; // candidate id
+        this.voteIndex = this.votesIndex && this.candidateId ? this.votesIndex[this.candidateId] : null; // individual vote index
+        this.vote = this.voteIndex ? this.votes[this.voteIndex] : null; // vote object
+        this.voteValue = this.vote ? parseInt(this.vote.value) : 0; // vote value
+        this.maxTokens = this.ballot ? parseInt(this.ballot.votes_limit) : 0;
+      }
+    }
   }
-}());
+
+
+  function updateVote() {
+    let oldValue = this.vote.value;
+    let diff = this.voteValue - oldValue;
+    let updatedRemainder = this.ballotTokens ? this.ballotTokens.points-diff : 0;
+    if((diff>0 && updatedRemainder >= 0) || (diff<0 && updatedRemainder <= this.maxTokens)) {
+      this.vote.value = this.voteValue+"";
+      this.ballotTokens.points = updatedRemainder;
+    } else {
+      this.voteValue = parseInt(this.vote.value);
+    }
+  }
+})();

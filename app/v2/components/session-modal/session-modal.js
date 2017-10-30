60e3ec98b993fa539088ef972fb7d6d732aabb02
Gustavo Vera Scuderi <gvescu@gmail.com>
2017-10-30 14:01:36 -0300

    Implementing session modal
    Signed-off-by: Gustavo Vera Scuderi <gvescu@gmail.com>

--- app/v2/components/session-modal/session-modal.js
+++ app/v2/components/session-modal/session-modal.js
@@ -17,26 +17,80 @@
       selector: 'sessionModal',
       controller: SessionModalCtrl,
       controllerAs: 'vm',
-      templateUrl: '/app/v2/components/session-modal/session-modal.html'
+      templateUrl: '/app/v2/components/session-modal/session-modal.html',
+      bindings: {
+        assembly: '<'
+      }
     });
 
-  Ctrl.$inject = [
-    '$scope', 'loginService', 'AppCivistAuth'
+    SessionModalCtrl.$inject = [
+    '$scope', 'loginService', 'AppCivistAuth', 'Notify'
   ];
 
-  function SessionModalCtrl($scope, LoginService, AppCivistAuth) {
+  function SessionModalCtrl($scope, LoginService, AppCivistAuth, Notify) {
     
-    function signup() {
-      if ($scope.isAnonymous) {
-        if (!$scope.user.email || !$scope.user.password) {
+    this.$onInit = () => {
+      this.user = {}
+    }
+
+    this.signup = () => {
+      window.Pace.restart();
+      console.log(this.user);
+      if (this.isAnonymous || this.isAnonymous === undefined) {
+        if (!this.user.email || !this.user.password) {
           Notify.show('Email and password are required', 'error');
+          window.Pace.stop();
           return;
         }
-        var rsp = AppCivistAuth.signUp().save($scope.user);
-        rsp.$promise.then(loginSuccess, loginError);
+        var rsp = AppCivistAuth.signUp().save(this.user);
+        rsp.$promise.then(signupSuccess, signupError);
       } else {
-        console.log("CLICKED!");
+        console.log("CLICK");
       }
+    }
+
+    this.signupSuccess = (user) => {
+      localStorageService.set('sessionKey', user.sessionKey);
+      localStorageService.set('authenticated', true);
+      localStorageService.set('user', user);
+      $scope.user = user;
+      if ($scope.user && $scope.user.language) {
+        $translate.use($scope.user.language);
+      }
+      user.assembly = $scope.assembly ? $scope.assembly : null;
+      loginService.loadAuthenticatedUserMemberships(user).then(function () {
+        var ongoingCampaigns = localStorageService.get('ongoingCampaigns');
+        var assembly = localStorageService.get('currentAssembly');
+
+        var rsp = Space.configs(assembly.resourcesResourceSpaceId).get();
+        rsp.$promise.then(function(data){
+          $scope.assemblyConfig = data;
+          window.Pace.stop();
+          if ($scope.assemblyConfig
+              && $scope.assemblyConfig['appcivist.assembly.instance.enable-homepage']
+              && $scope.assemblyConfig['appcivist.assembly.instance.enable-homepage'] === 'TRUE') {
+            $state.go('v2.assembly.aid.home', { aid: assembly.assemblyId }, { reload: true });
+          } else {
+            let campaign = ongoingCampaigns ? ongoingCampaigns[0] : null
+            $state.go('v2.assembly.aid.campaign.cid', { aid: assembly.assemblyId, cid: campaign.campaignId }, { reload: true });
+          }
+
+        }, function(error) {
+            window.Pace.stop();
+            Notify.show('Error while trying to fetch assembly config', 'error');
+        });
+      });
+    }
+
+    this.signupError = () => {
+      window.Pace.stop();
+      console.log(error);
+      var msg = 'Error while trying to authenticate to the server';
+
+      if (error && error.data && error.data.statusMessage) {
+        msg = error.data.statusMessage;
+      }
+      Notify.show(msg, 'error');
     }
 
   }

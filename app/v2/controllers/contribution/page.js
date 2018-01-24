ac202f8e415137db02daf6bac4ba0bb3c9921066
Gustavo Vera Scuderi <gvescu@gmail.com>
2018-01-24 14:30:22 -0300

    Adding fields
    Signed-off-by: Gustavo Vera Scuderi <gvescu@gmail.com>

--- app/v2/controllers/contribution/page.js
+++ app/v2/controllers/contribution/page.js
@@ -262,6 +262,7 @@
         function (data) {
           data.informalScore = Contributions.getInformalScore(data);
           $scope.proposal = data;
+          console.log(data);
           localStorageService.set('currentContribution',$scope.proposal);
           $scope.proposal.frsUUID = data.forumResourceSpaceUUID;
           var workingGroupAuthors = data.workingGroupAuthors;

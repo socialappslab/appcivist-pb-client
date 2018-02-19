9ca1c00cbcb8e2af527f16c20086d79da272076a
Gustavo Vera Scuderi <gvescu@gmail.com>
2018-02-19 15:39:26 -0300

    Added style for the prompt
    Signed-off-by: Gustavo Vera Scuderi <gvescu@gmail.com>

--- app/v2/components/motivational-messages/motivational-messages.js
+++ app/v2/components/motivational-messages/motivational-messages.js
@@ -10,24 +10,59 @@
      *
      * @example
      *
-     *  <motivational-messages></motivational-messages>
+     *  <motivational></motivational>
      */
     appCivistApp
       .component('motivational', {
         selector: 'motivational',
         bindings: {
+            // values = { 'commentbox', 'attachments', 'contributing', 'follow' }
+            page: '@',
+            resource: '='
         },
-        controller: TopbarLangpickerCtrl,
+        controller: MotivationalCtrl,
         controllerAs: 'vm',
         templateUrl: '/app/v2/components/motivational-messages/motivational-messages.html'
       });
   
-      TopbarLangpickerCtrl.$inject = [
-        '$scope', '$translate', 'localStorageService', 'LocaleService'
+      MotivationalCtrl.$inject = [
+        '$scope', '$translate', 'localStorageService', 'LocaleService', 'Notify'
     ];
   
-    function TopbarLangpickerCtrl($scope, $translate, localStorageService, LocaleService) {
-      
+    function MotivationalCtrl($scope, $translate, localStorageService, LocaleService, Notify) {
+
+        let commentBoxMessages = ['motivational.commentbox.message1', 'motivational.commentbox.message2', 'motivational.commentbox.message3', 'motivational.commentbox.message4', 'motivational.commentbox.message5', 'motivational.commentbox.message6'];
+        let contributingMessages = ['motivational.contributing.message1', 'motivational.contributing.message2', 'motivational.contributing.message3'];
+        let attachmentsMessages = ['motivational.attachments.message1', 'motivational.attachments.message2', 'motivational.attachments.message3', 'motivational.attachments.message4'];
+        let followMessages = ['motivational.follow.message1', 'motivational.follow.message2', 'motivational.follow.message3'];
+        
+        this.$onInit = () => {
+            setTimeout(() => {
+                switch (this.page) {
+                    case 'campaign':
+                        break;
+                    case 'wg':
+                        break;
+                    case 'contribution':
+                        break;
+                }
+                $scope.message = commentBoxMessages[0];
+                document.getElementsByTagName('motivational')[0].classList.add('show');
+            }, 10000)
+        }
+
+        this.hideMessage = () => {
+            document.getElementsByTagName('motivational')[0].classList.remove('show');
+        }
+
+        this.turnOff = () => {
+            let turnoff = confirm('Are you sure you don\'t want to receive this suggestions anymore?');
+            if (turnoff) {
+                // TODO: turn off in profile
+                this.hideMessage();
+                Notify.show('You won\'t see more suggestions', 'success');
+            }
+        }
     }
   
   }());

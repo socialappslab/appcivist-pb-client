5ef86d7a7af58e9a27e8ac3e1c94aae7e7a2f84f
Yohanna Lisnichuk <yohanitalisnichuk@gmail.com>
2019-09-28 16:41:17 -0400

    Update gitignore and add deploy.sh
    Signed-off-by: Yohanna Lisnichuk <yohanitalisnichuk@gmail.com>

---
+++ docker_confs/deploy.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+source /environment
+cd /appcivist-pb-client/app
+if [ -e env.js ]
+then
+    echo "Using existing env.js"
+else
+    echo "Using sample env.sample.js"
+    cp env.sample.js env.js
+fi
+
+sed -i -e "s/SENTRY_SECRET/$SENTRY_SECRET/g" env.js
+sed -i -e "s/SENTRY_PROJECT_ID/$SENTRY_PROJECT_ID/g" env.js
+
+cd /appcivist-pb-client
+bower install --allow-root
+npm install
+grunt build
+cp -rf dist/* /var/www/html/appcivist-pb
+
+
